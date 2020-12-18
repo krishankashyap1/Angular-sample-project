8354b56bcd3975edc43a3aefedba595f5838590c
krishkash92 <37514078+krishkash92@users.noreply.github.com>
2020-12-18 19:33:08 +0530

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
     res.sendFile(`dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+}); */
+
+app.use(express.static(’./dist/kelton’));
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+);
 });
+
+app.listen(process.env.PORT || 8080);
